109181f0024d5bf0edcb8d1a10d0a1cbb320e04a
slavinserg4 <slavinserg4@gmail.com>
2024-10-21 00:20:56 +0300

    hw4-part1

---
+++ src/routes/routes.tsx
@@ -0,0 +1,10 @@
+import {createBrowserRouter} from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout/>,
+        children:[]
+    }
+]);
