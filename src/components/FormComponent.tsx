ac9977b964df8610aadb2f6edace4eab33c1cd61
slavinserg4 <slavinserg4@gmail.com>
2024-10-22 23:59:27 +0300

    hw5-part1

---
+++ src/components/FormComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FormComponent = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default FormComponent;
---
+++ src/services/api.services.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const axiosInstanse = axios.create({
+    baseURL:'https://jsonplaceholder.typicode.com',
+    headers:{}
+})
+
+
+
+export const apiService ={
+    post: {
+
+    }
+}
