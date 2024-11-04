ec08ba6bc6799ed53460ed368ee41c9eb034b35c
slavinserg4 <slavinserg4@gmail.com>
2024-11-04 13:21:39 +0200

    hw8-task1-finish

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Menu from "../components/menu/Menu";
 import {Outlet} from "react-router-dom";
+import {IUser} from "../models/IUser";
+import {IPost} from "../models/IPost";
+import {IComment} from "../models/IComment";
+import {getComments, getPosts, getUsers} from "../services/api.services";
+import {MyContext} from "../ContextProvider";
 
 
 const MainLayout = () => {
+    const [users, setUsers] = useState<IUser[]>([])
+    const [posts, setPosts] = useState<IPost[]>([])
+    const [comments, setComments] = useState<IComment[]>([])
+    useEffect(() => {
+        const allInfo = async () =>{
+            let userResponse = await getUsers();
+            let postResponse = await getPosts();
+            let commentResponse = await getComments()
+
+            setUsers(userResponse);
+            setPosts(postResponse);
+            setComments(commentResponse);
+        }
+        allInfo();
+    }, []);
     return (
         <div>
-            <Menu/>
-            <hr/>
-            <Outlet/>
+            {
+                <MyContext.Provider value={{
+                    UserSlice: {
+                        allUsers: users,
+                        user: null
+                    },
+                    PostSlice: {
+                        allPosts: posts,
+                        post: null
+                    },
+                    CommentSlice: {
+                        allComments: comments,
+                        comment: null
+                    }
+                }}>
+                    <Menu/>
+                    <hr/>
+                    <Outlet/>
+                </MyContext.Provider>
+            }
         </div>
     );
 };
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,41 +6,12 @@ import {getComments, getPosts, getUsers} from "../services/api.services";
 import {MyContext} from "../ContextProvider";
 
 const HomePage = () => {
-    const [users, setUsers] = useState<IUser[]>([])
-    const [posts, setPosts] = useState<IPost[]>([])
-    const [comments, setComments] = useState<IComment[]>([])
-    useEffect(() => {
-        const allInfo = async () =>{
-            let userResponse = await getUsers();
-            let postResponse = await getPosts();
-            let commentResponse = await getComments()
 
-            setUsers(userResponse);
-            setPosts(postResponse);
-            setComments(commentResponse);
-        }
-        allInfo();
-    }, []);
     
     return (
         <div>
             Home Page
-            {
-                <MyContext.Provider value={{
-                    UserSlice: {
-                        allUsers: users,
-                        user: null
-                    },
-                    PostSlice: {
-                        allPosts: posts,
-                        post: null
-                    },
-                    CommentSlice: {
-                        allComments: comments,
-                        comment: null
-                    }
-                }}></MyContext.Provider>
-            }
+
         </div>
     );
 };
